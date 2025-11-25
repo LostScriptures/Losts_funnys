3f250b6d0d605d0c3012504ec32c959e9e926613
LostScriptures <lost.player.game@gmail.com>
2025-11-25 19:30:22 +0100

    ok, first commit with the vrc tracker

---
+++ modules/minigame.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+
+def do_minigame(self, args):
+    '''
+    Currently in developement
+    Usage: minigame
+    '''
+    ...
+
+def help_minigame(self, args):
+    print(do_minigame.__doc__)
